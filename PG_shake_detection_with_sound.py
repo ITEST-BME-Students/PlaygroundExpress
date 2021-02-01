5e04d505788d0f0b585d5996e114fd8c4fba61f1
Dieter Vanderelst <34309937+dvanderelst@users.noreply.github.com>
2021-02-01 11:46:28 -0500

    Create PG_shake_detection_with_sound.py

---
+++ PG_shake_detection_with_sound.py
@@ -0,0 +1,20 @@
+from adafruit_circuitplayground import cp
+import time
+
+
+
+from adafruit_circuitplayground import cp
+import time
+
+while True:
+    time.sleep(0.1)
+    x, y, z = cp.acceleration
+    acceleration = (x ** 2 + y ** 2 + z **2) ** 0.5
+    print(acceleration)
+    if acceleration > 20:
+        for i in range(3):
+            cp.pixels.fill((255, 0, 0))
+            cp.play_tone(500, 0.25)
+     
+            cp.pixels.fill((0, 0, 0))
+            cp.play_tone(250, 0.25)
